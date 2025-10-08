2e104882555f6ca7bf0f3f9d32b119c499fbdca1
Jake Shadle <jake.shadle@embark-studios.com>
2025-10-08 14:22:57 +0200

    Remove cache

--- crates/corro-types/src/pubsub.rs
+++ crates/corro-types/src/pubsub.rs
@@ -563,7 +563,7 @@ impl DbName {
 
             Self::Path(pb)
         } else {
-            Self::Memory(format!("file:{id}?mode=memory&cache=shared"))
+            Self::Memory(format!("file:{id}?mode=memory"))
         }
     }
 
