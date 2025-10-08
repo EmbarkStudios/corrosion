bbbf4e598cfdac4381d3915171ee22e221fb109f
Jake Shadle <jake.shadle@embark-studios.com>
2025-10-08 14:24:05 +0200

    Nvm

--- crates/corro-types/src/pubsub.rs
+++ crates/corro-types/src/pubsub.rs
@@ -563,7 +563,7 @@ impl DbName {
 
             Self::Path(pb)
         } else {
-            Self::Memory(format!("file:{id}?mode=memory"))
+            Self::Memory(format!("file:{}?mode=memory&cache=shared", id.as_simple()))
         }
     }
 
